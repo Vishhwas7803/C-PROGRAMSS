179458f5520d49808e24ac7d134d36e2e1dea465
Vishhwas7803 <122535452+Vishhwas7803@users.noreply.github.com>
2023-01-12 20:54:31 +0530

    Create 5. C program to find power and square root of any number.c

---
+++ 5. C program to find power and square root of any number.c
@@ -0,0 +1,9 @@
+
+// C program for the above approach
+#include <math.h>
+#include <stdio.h>
+// Function to find the square-root of N
+double findSQRT(double N) { return sqrt(N); }
+// Driver Code
+int main()
+{
