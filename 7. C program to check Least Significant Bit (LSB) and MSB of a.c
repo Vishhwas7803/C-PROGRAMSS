36561b29b92080b8984982d18642d67cb746c68d
Vishhwas7803 <122535452+Vishhwas7803@users.noreply.github.com>
2023-01-12 20:59:31 +0530

    Create 7. C program to check Least Significant Bit (LSB) and MSB of a.c

---
+++ 7. C program to check Least Significant Bit (LSB) and MSB of a.c
@@ -0,0 +1,14 @@
+
+number using bitwise operator.
+#include<stdio.h>
+Int main ()
+{
+int num,lsb,msb;
+printf("Enter a number: ");
+scanf("%d", &num);
+lsb = num & 1;
+msb = num >> 31;
+printf("Least Significant Bit: %d\n", lsb);
+printf("Most Significant Bit: %d\n", msb);
+return 0;
+}
